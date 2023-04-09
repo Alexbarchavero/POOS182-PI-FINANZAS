d76a98cb7d09e94ec88128c29b8af9c5198f1126
Alexbarchavero <125913095+Alexbarchavero@users.noreply.github.com>
2023-04-08 21:41:10 -0600

    Restructuracion de codigo
    Se añadieron las siguientes pestañas en la ventana 1:
    - Actualizar datos
    - Eliminar cuenta
    Ademas se modificaron y crearon algunos metodos en la clase usuarios

--- Main/usuarios.py
+++ Main/usuarios.py
@@ -18,21 +18,7 @@ class usuarios:
         except sqlite3.OperationalError:
             print("Error de conexion")
     
-    def obtenerId(self,nombre):
-        try:
-            conx = self.conexionDB()
-            c0 = conx.cursor()
-            datos = (nombre,)
-            consultaObtenerId = "SELECT id FROM tbUsuarios where nombre = ?"
-            c0.execute(consultaObtenerId,datos)
-            resultado = c0.fetchone()
-            if resultado:
-                ide = resultado[0]
-            else:
-                print("No existe el Usuario")
-            return ide
-        except sqlite3.OperationalError:
-            print("Error de consulta")
+    # -------------------------------------------------- Funciones de la ventana 1 -------------------------------------------------- #
     
     def signup(self):
         try:
@@ -68,20 +54,105 @@ class usuarios:
                 c2.execute(consultaLogin,datos)
                 resultado = c2.fetchone()
                 if resultado:
-                    self.__username__ = nombre
                     messagebox.showinfo("Exito","Inicio de sesión exitoso")
-                    self.r = True
+                    r = True
                 else:
                     messagebox.showerror("Error","Usuario o contraseña incorrectos")
-                    self.r = False
+                    r = False
                 conx.close()
-                return self.r
+                return r
         except sqlite3.OperationalError:
             print("Error de consulta")
     
-    def logout(self):
-        self.__username__ = None
-        print("Cierre de sesion exitoso")
+    def updateInfo(self, name, password):
+        try:
+            conx = self.conexionDB()
+            nombre = self.__nombre__.get()
+            contra = self.__contra__.get()
+            c6 = conx.cursor()
+            if (nombre=="" or contra==""):
+                messagebox.showwarning("Advertencia","Faltan datos por ingresar")
+                conx.close()
+            else:
+                def validar():
+                    try:
+                        datos1 = (nombre,contra)
+                        sql1 = "SELECT nombre, contrasena FROM tbUsuarios WHERE nombre = ? and contrasena = ?"
+                        c6.execute(sql1,datos1)
+                        resultado = c6.fetchone()
+                        if resultado:
+                            conx.commit()
+                            return resultado
+                    except sqlite3.OperationalError:
+                        print("Error de consulta")
+                if validar() is None:
+                    messagebox.showerror(
+                        "Error",
+                        "El nombre y/o la contraseña proporcionados no son correctos o no coinciden en la base de datos"
+                    )
+                    conx.close()
+                else:
+                    resultado = messagebox.askyesno("Confirmacion","¿Esta seguro de actualizar sus datos?")
+                    if resultado:
+                        datos = (name,password,nombre)
+                        consultaUpdateInfo = "UPDATE tbUsuarios SET nombre = ?, contrasena = ? WHERE nombre = ?"
+                        c6.execute(consultaUpdateInfo,datos)
+                        conx.commit()
+                        conx.close()
+                        messagebox.showinfo("Exito","Datos actualizados")
+                    else:
+                        messagebox.showinfo("Informacion","Datos no actualizados")
+                        conx.close()
+        except sqlite3.OperationalError:
+            print("Error de consulta")
+    
+    def deleteAccount(self):
+        try:
+            conx = self.conexionDB()
+            nombre = self.__nombre__.get()
+            contra = self.__contra__.get()
+            c7 = conx.cursor()
+            if (nombre=="" or contra==""):
+                messagebox.showwarning("Advertencia","Faltan datos")
+            else:
+                def validar():
+                    try:
+                        datos1 = (nombre,contra)
+                        sql1 = "SELECT nombre, contrasena FROM tbUsuarios WHERE nombre = ? and contrasena = ?"
+                        c7.execute(sql1,datos1)
+                        resultado = c7.fetchone()
+                        if resultado:
+                            conx.commit()
+                            return resultado
+                    except sqlite3.OperationalError:
+                        print("Error de consulta")
+                
+                if validar() is None:
+                    messagebox.showerror("Error","Los datos son incorrectos o no coinciden con algun usuario registrado")
+                    conx.close()
+                else:
+                    def conseguirID():
+                        data = (nombre,contra)
+                        conseguirIDEsql = "SELECT id FROM tbUsuarios WHERE nombre = ? and contrasena = ?"
+                        c7.execute(conseguirIDEsql,data)
+                        resultado = c7.fetchone()
+                        conx.commit()
+                        return resultado
+                    ide = conseguirID()
+                    answer = messagebox.askyesno("Confirmacion","¿Desea eliminar su cuenta?")
+                    if answer:
+                        consultaEliminarCuenta = "DELETE FROM tbUsuarios WHERE id = ?"
+                        c7.execute(consultaEliminarCuenta,ide)
+                        conx.commit()
+                        conx.close()
+                        messagebox.showinfo("Exito","La cuenta ha sido eliminada correctamente")
+                    else:
+                        messagebox.showwarning("Advertencia","La cuenta no ha sido eliminada")
+                        conx.close()
+        except sqlite3.OperationalError:
+            print("Error de consulta")
+    
+    # -------------------------------------------------- Funciones de la ventana 2 -------------------------------------------------- #
     
     def addTransaccion(self,categoria):
         try:
@@ -118,9 +189,8 @@ class usuarios:
             return registros
         except sqlite3.OperationalError:
             print("Error de consulta")
-    def showUserinfo(self):
-        try:
-            nombre = self.__nombre__.get()
-        except:
-            print("No jalo")
-#Según los 2 archivos que te acabo de proporcionar, 
+    
+    def logout(self):
+        self.__nombre__ = None
+        self.__contra__ = None
+        messagebox.showinfo("Cerrar sesion","Cierre de sesion exitoso")
