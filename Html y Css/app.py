0f666cb7f5d2b16e592609f620033d6c23fe1ae0
Alexbarchavero <125913095+Alexbarchavero@users.noreply.github.com>
2023-07-12 15:36:28 -0600

    Avance Proyecto

--- Html y Css/app.py
+++ Html y Css/app.py
@@ -13,18 +13,63 @@ mysql = MySQL(app)
 def inicio():
     return render_template('inicio.html')
 
-@app.route("/guardar", methods=['POST'])
-def guardar():
+@app.route("/finanzas", methods = ['POST'])
+def finanzas():
+    if request.method == 'POST':
+        ISnombre = request.form['nombre-login']
+        IScontrasena = request.form['contrasena-login']
+        
+        cis = mysql.connection.cursor()
+        cis.execute('select * from tbusuarios where nombre = %s and contrasena = %s',(ISnombre, IScontrasena))
+        consulta = cis.fetchone()
+        mysql.connection.commit()
+        if consulta is not None:
+            flash("Inicio de sesion correcto!")
+            return render_template('finanzas.html')
+        else:
+            flash("Inicio de sesion incorrecto!")
+            return render_template('inicio.html')
+
+@app.route("/registrar", methods=['POST'])
+def registrar():
     if request.method == 'POST':
         Vnombre=request.form['nombre']
         Vcontrasena=request.form['contrasena']
         Vcuenta=request.form['cuenta']
         
-        cs = mysql.connection.cursor()
-        cs.execute('insert into tbUsuarios (nombre, contrasena, cuenta) values (%s, %s, %s)', (Vnombre,Vcontrasena,Vcuenta))
+        cr = mysql.connection.cursor()
+        cr.execute('insert into tbusuarios (nombre, contrasena, cuenta) values (%s, %s, %s)', (Vnombre,Vcontrasena,Vcuenta))
         mysql.connection.commit()
-
     flash('Usuario registrado correctamente')
+    return redirect(url_for('inicio'))
+
+
+
+@app.route("/actualizar", methods=['POST'])
+def actualizar():
+    if request.method == 'POST':
+        Anombrea = request.form['nombre-actual']
+        Acontrasenaa = request.form['contrasena-actual']
+        Anombren = request.form['nuevo-nombre']
+        Acontrasenan = request.form['nueva-contrasena']
+        Acuentan = request.form['nueva-cuenta']
+        
+        ca = mysql.connection.cursor()
+        ca.execute('update tbusuarios set nombre = %s, contrasena = %s, cuenta = %s where nombre = %s and contrasena = %s', (Anombren, Acontrasenan, Acuentan, Anombrea, Acontrasenaa))
+        mysql.connection.commit()
+    flash('Usuario actualizado correctamente')
+    return redirect(url_for('inicio'))
+
+@app.route("/eliminar", methods=['POST'])
+def eliminar():
+    if request.method == 'POST':
+        Enombre = request.form['nombre-eliminar']
+        Econtrasena = request.form['contrasena-eliminar']
+        
+        ce = mysql.connection.cursor()
+        ce.execute('delete from tbusuarios where nombre = %s and contrasena = %s',(Enombre, Econtrasena))
+        mysql.connection.commit()
+    flash('Usuario eliminado correctamente')
     return redirect(url_for('inicio'))
 
 if __name__ == '__main__':
